134ad6917574bcbffc6355d8924ab7a0bcec59f8
Tane4ka170 <tane4ka170@gmail.com>
2025-01-27 18:04:39 +0200

    Enhanced Header with Clerk UserButton and navigation

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -3,19 +3,31 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { useUser } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 function Header() {
   const { user, isSignedIn } = useUser();
   return (
     <div className="p-5 flex justify-center items-center border shadow-sm">
-      <div>
-        <span>FinanSmart</span>
+      <div className="flex flex-row items-center">
+        <Image src={"/finance.svg"} alt="logo" width={20} height={20} />
+        <span className="text-lime-800 font-bold">FinanSmart</span>
       </div>
-      <div>
-        <button>1</button>
-        <button>2</button>
-      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3 items-center">
+          <Link href="/dashboard">
+            <Button variant="outline" className="rounded-full">
+              Dashboard
+            </Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button className="rounded-full">Get started</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
