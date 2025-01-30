1399e5a8226fdc447ab53b688bf43be810b1187b
Tane4ka170 <tane4ka170@gmail.com>
2025-01-30 17:41:54 +0200

    Configured Drizzle ORM

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_K0rCIOQZhn2v@ep-odd-bonus-a5ar37dy-pooler.us-east-2.aws.neon.tech/finan-smart?sslmode=require",
+    connectionStrings: [
+      "postgresql://neondb_owner:npg_K0rCIOQZhn2v@ep-odd-bonus-a5ar37dy-pooler.us-east-2.aws.neon.tech/finan-smart?sslmode=require",
+    ],
+  },
+};
