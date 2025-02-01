1eb8e7baadd44ca2ae71432edcf613c59fcbed02
Tane4ka170 <tane4ka170@gmail.com>
2025-02-01 16:50:24 +0200

    Implemented budget check on dashboard load

--- src/app/(routes)/dashboard/layout.jsx
+++ src/app/(routes)/dashboard/layout.jsx
@@ -1,2 +1,25 @@
 "use client";
-import React from "react";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { db } from "../../../../utils/dbConfig";
+import { Budgets } from "../../../../utils/schema";
+import { eq } from "drizzle-orm";
+
+function DashboardLayout({ children }) {
+  const { user } = useUser();
+  const router = useRouter();
+
+  const checkUserBudgets = async () => {
+    const result = await db.select
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
+    if (result?.length === 0) {
+      router.replace("/dashboard/budgets");
+    }
+  };
+
+  useEffect(() => {
+    user && checkUserBudgets();
+  }, [user]);
+}
