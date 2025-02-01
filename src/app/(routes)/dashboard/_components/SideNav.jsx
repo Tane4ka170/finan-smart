06c2fe69a94f3f846ebc253efa58094cbb07bbd0
Tane4ka170 <tane4ka170@gmail.com>
2025-02-01 16:49:28 +0200

    Added navigation links to SideNav

--- src/app/(routes)/dashboard/_components/SideNav.jsx
+++ src/app/(routes)/dashboard/_components/SideNav.jsx
@@ -6,6 +6,7 @@ import {
   ShieldCheck,
 } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect } from "react";
 
@@ -54,6 +55,20 @@ function SideNav() {
       <div className="flex flex-row items-center">
         <Image src={"./finance.svg"} alt="logo" width={40} height={25} />
         <span className="text-lime-800 font-bold text-xl">FinanSmart</span>
+      </div>
+
+      <div className="mt-5">
+        {menuList.map((menu, index) => (
+          <Link href={menu.path} key={index}>
+            <h2
+              className={`flex gap-2 items-center text-gray-500 font-medium mb-2 p-4 cursor-pointer rounded-full hover:text-primary hover:bg-lime-100 ${
+                path === menu.path && "text-primary bg-lime-100"
+              }`}
+            >
+              <menu.icon /> {menu.name}
+            </h2>
+          </Link>
+        ))}
       </div>
     </div>
   );
