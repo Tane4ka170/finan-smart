b8acc931f99834fb9b3e33022717360700836dd8
Tane4ka170 <tane4ka170@gmail.com>
2025-02-02 16:05:19 +0200

    Added dashboard content

--- src/app/(routes)/dashboard/page.jsx
+++ src/app/(routes)/dashboard/page.jsx
@@ -1,2 +1,21 @@
 "use client";
+import { useUser } from "@clerk/nextjs";
 import React from "react";
+import CardInfo from "./_components/CardInfo";
+
+function Dashboard() {
+  const { user } = useUser();
+  return (
+    <div className="p-8">
+      <h2 className="font-bold text-4xl">Hi, {user?.fullName}</h2>
+      <p className="text-gray-500">
+        Here's what's going on with your finances—let's take control of your
+        expenses!
+      </p>
+
+      <CardInfo />
+    </div>
+  );
+}
+
+export default Dashboard;
