50f8d046e698f3fb9d8adb5762d20390016078d6
Tane4ka170 <tane4ka170@gmail.com>
2025-02-03 17:06:46 +0200

    Enhanced CardInfo with UI & basic logic

--- src/app/(routes)/dashboard/_components/CardInfo.jsx
+++ src/app/(routes)/dashboard/_components/CardInfo.jsx
@@ -1,4 +1,6 @@
+import { Sparkles } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import formatNumber from "../../../../../utils";
 
 function CardInfo({ budgetList, incomeList }) {
   const [totalBudget, setTotalBudget] = useState(0);
@@ -42,11 +44,42 @@ function CardInfo({ budgetList, incomeList }) {
     setTotalIncome(totalIncome_);
   };
 
-    return <div>{budgetList.length > 0 ? (<div>
-        <div className="p-7 border mt-4 rounded-2xl flex items-center justify-between">
-            <div></div>
-      </div>
-  </div>) : ()}</div>;
+  return (
+    <div>
+      {budgetList.length > 0 ? (
+        <div>
+          <div className="p-7 border mt-4 rounded-2xl flex items-center justify-between">
+            <div className="">
+              <div className="fle mb-2 flex-row space-x-1 justify-between">
+                <h2>Finan Smart AI</h2>
+                <Sparkles
+                  className="rounded-full text-white w-10 h-10 p-2  bg-gradient-to-r
+    from-lime-500
+    via-green-500
+    to-emerald-500
+    background-animate "
+                />
+              </div>
+              <h2 className="font-light text-md ">
+                {financialAdvise || "Fetching your financial insights..."}
+              </h2>
+            </div>
+          </div>
+
+          <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div className="p-7 border rounded-2xl flex items-center justify-between">
+              <div>
+                <h2>Total Budget</h2>
+                <h2>${formatNumber(totalBudget)}</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 }
 
 export default CardInfo;
