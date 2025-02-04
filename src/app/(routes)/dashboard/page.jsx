63c6475fef152dfc0fa763691b5f611322d2e047
Tane4ka170 <tane4ka170@gmail.com>
2025-02-04 16:51:38 +0200

    Enhanced Dashboard with data fetching and logic

--- src/app/(routes)/dashboard/page.jsx
+++ src/app/(routes)/dashboard/page.jsx
@@ -1,10 +1,72 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardInfo from "./_components/CardInfo";
+import { db } from "../../../../utils/dbConfig";
+import { Budgets, Expenses, Incomes } from "../../../../utils/schema";
+import { eq, sql } from "drizzle-orm";
 
 function Dashboard() {
   const { user } = useUser();
+  const [budgetList, setBudgetList] = useState([]);
+  const [incomeList, setIncomeList] = useState([]);
+  const [expensesList, setExpensesList] = useState([]);
+
+  const getBudgetList = async () => {
+    const result = await db
+      .select({
+        ...getTableColumns(Budgets),
+
+        totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+        totalItem: sql`count(${Expenses.id})`.mapWith(Number),
+      })
+      .from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .groupBy(Budgets.id)
+      .orderBy(desc(Budgets.id));
+
+    setBudgetList(result);
+    getAllExpenses();
+  };
+
+  const getAllExpenses = async () => {
+    const result = await db
+      .select({
+        id: Expenses.id,
+        name: Expenses.name,
+        amount: Expenses.amount,
+        createdAt: Expenses.createdAt,
+      })
+      .from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress))
+      .orderBy(desc(Expenses.id));
+    setExpensesList(result);
+  };
+
+  const getIncomeList = async () => {
+    try {
+      const result = await db
+        .select({
+          ...getTableColumns(Incomes),
+          totalAmount: sql`SUM(CAST(${Incomes.amount} AS NUMERIC))`.mapWith(
+            Number
+          ),
+        })
+        .from(Incomes)
+        .groupBy(Incomes.id);
+
+      setIncomeList(result);
+    } catch (error) {
+      console.error("Error fetching income list:", error);
+    }
+  };
+
+  useEffect(() => {
+    user && getBudgetList();
+  }, [user]);
+
   return (
     <div className="p-8">
       <h2 className="font-bold text-4xl">Hi, {user?.fullName}</h2>
@@ -13,7 +75,7 @@ function Dashboard() {
         expenses!
       </p>
 
-      <CardInfo />
+      <CardInfo budgetList={budgetList} incomeList={incomeList} />
     </div>
   );
 }
