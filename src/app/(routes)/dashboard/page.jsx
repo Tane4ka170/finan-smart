584b57195ac7d7ab309034e94c15c73701df3c26
Tane4ka170 <tane4ka170@gmail.com>
2025-02-05 16:08:00 +0200

    Added BarChartDashboard and ExpensesListTable

--- src/app/(routes)/dashboard/page.jsx
+++ src/app/(routes)/dashboard/page.jsx
@@ -5,6 +5,7 @@ import CardInfo from "./_components/CardInfo";
 import { db } from "../../../../utils/dbConfig";
 import { Budgets, Expenses, Incomes } from "../../../../utils/schema";
 import { eq, sql } from "drizzle-orm";
+import BarChartDashboard from "./_components/BarChartDashboard";
 
 function Dashboard() {
   const { user } = useUser();
@@ -76,6 +77,26 @@ function Dashboard() {
       </p>
 
       <CardInfo budgetList={budgetList} incomeList={incomeList} />
+      <div className="grid grid-cols-1 lg:grid-cols-3 mt-6 gap-5">
+        <div className="lg:col-span-2">
+          <BarChartDashboard budgetList={budgetList} />
+          <ExpensesListTable
+            expensesList={expensesList}
+            refreshData={() => getBudgetList()}
+          />
+        </div>
+
+        <div className="grid gap-5">
+          <h2 className="font-bold text-lg">Latest Budgets</h2>
+          {budgetList?.length > 0
+            ? budgetList.map((budget, index) => (
+                <BudgetItem budget={budget} key={index} />
+              ))
+            : [1, 2, 3, 4].map((items, index) => (
+                <div className="h-[180xp] w-full bg-slate-200 rounded-lg animate-pulse"></div>
+              ))}
+        </div>
+      </div>
     </div>
   );
 }
